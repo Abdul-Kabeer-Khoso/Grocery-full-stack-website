a18a0e49b42ffff508f0c4889e75bd3b971f121f
Abdul Kabeer <abdulkabeerkhoso082@gmail.com>
2025-05-17 09:12:59 +0500

    add products to db and show product from db

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use('/public', express.static(path.join(__dirname, "public")));
 app.use(express.static('public'));
 
 
+//uuid 
+const {v4: uuidv4} = require('uuid');
+
+
 //MySQL setup
 const mysql = require('mysql2');
 
@@ -31,9 +35,40 @@ app.get("/product/:id", (req, res)=>{
     res.render("relatedProducts.ejs");
 })
 
+//Seller
 app.get("/seller", (req, res)=>{
-    res.render("seller.ejs");
+    let allProducts = 'select * from Products';
+    try{
+      connection.query(allProducts, (err, result)=>{
+        res.render('seller.ejs', {result})
+      })
+    }catch(err){
+        res.send(err);
+    }
+
 })
+
+app.post("/seller/add", (req, res)=>{
+    let {product} = req.body;
+    let productId = uuidv4();
+    let addQuery = "INSERT INTO Products (productId, productName, productCategory, productImage1, productReviews, productDescription, oldPrice, newPrice, inStock) VALUES (?, ?, ?, ?, ?, ?, ?, ?, TRUE);"
+    try{
+        connection.query(addQuery, 
+            [productId, product.productName, product.productCategory, product.image1URL, '4', product.productDescription, product.productOldPrice, product.productNewPrice, ], 
+            (err, result)=>{
+                 if(err){
+                    res.send("There is something error in add product query");
+                 }   
+                 else{
+                    res.redirect('/seller');
+                 }
+            })
+    }catch(err){
+        res.send(err);
+    }
+})
+
+
 
 app.listen(8080, ()=>{
     console.log("App is listening");
