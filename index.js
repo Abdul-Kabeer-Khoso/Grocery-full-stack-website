20c0a6d91972010770b04ee99fbab13607f8b594
Abdul Kabeer <abdulkabeerkhoso082@gmail.com>
2025-05-23 20:26:55 +0500

    Add total products in cart feature

--- index.js
+++ index.js
@@ -32,23 +32,32 @@ const connection = mysql.createConnection({
 
 //Home Page
 app.get("/product", (req, res)=>{
-    res.render('Products.ejs');
+    let productsNumberQuery = 'select * from Cart';
+    try{
+        connection.query(productsNumberQuery, (err, result)=>{
+            if(err){
+                res.send("There is something error in products Number Query");
+            } else{
+                res.render('products.ejs', {totalCartProducts: result.length});
+            }
+        })
+    } catch(err){
+        res.send("There is something error in home page")
+    }
 });
 
 //show all products
-app.get("/product/all", (req, res)=>{
+app.get("/product/all", async (req, res)=>{
     let showProductQuery = 'select * from Products';
+    let productsNumberQuery = 'select * from Cart';
     try{
-        connection.query(showProductQuery, (err, result)=>{
-            let products = result;
-            if(err){
-                res.send(err);
-            } else{
-                res.render('allProducts.ejs', {products});
-            }
-        })
-    }catch(err){
-        res.send(err);
+        let [myProducts] = await connection.promise().query(showProductQuery);
+        let [productsNumber] = await connection.promise().query(productsNumberQuery);
+        let allProducts = myProducts;
+        let totalCartProducts = productsNumber.length;
+        res.render('allProducts.ejs', {products: allProducts, totalCartProducts: totalCartProducts});
+        }catch(err){
+        res.send("There is something error in showing all products");
     }
 })
 
@@ -64,6 +73,7 @@ app.get("/product/cart", (req, res)=>{
                 totalPricePerItem: totalPricePerItem
             };
             });
+            const productLength = products.length;
             const prices = products.map(product=> product.totalPricePerItem);
             const price = prices.reduce((sum, price)=> sum + price, 0);
             const tax = price * 0.02;
@@ -71,7 +81,7 @@ app.get("/product/cart", (req, res)=>{
             if(err){
                 res.send("There is error in query of showing cart product");
             } else{
-                res.render('cart.ejs', {products: products, price: price, tax: tax, totalPrice: totalPrice });
+                res.render('cart.ejs', {products: products, price: price, tax: tax, totalPrice: totalPrice, totalCartProducts: productLength });
             }
         })
     } catch (err){
@@ -88,14 +98,17 @@ app.get("/product/address", (req, res)=>{
 app.get("/product/:id", async (req, res)=>{
     let {id}= req.params;
     let findProductQuery = `select * from Products where productId = "${id.toString()}"`;
-    let findRelatedProductsQuery = 'select * from Products where productCategory = ?'
+    let findRelatedProductsQuery = 'select * from Products where productCategory = ?';
+    let productNumberQuery = 'select * from Cart';
     try{
         let productDetails =await connection.promise().query(findProductQuery);
         let myProductDetails = productDetails[0][0];
         let relatedProducts = await connection.promise().query(findRelatedProductsQuery, [myProductDetails.productCategory]);
         let myRelatedProducts = relatedProducts[0];
+        let productNumber = await connection.promise().query(productNumberQuery);
+        let totalCartProducts = productNumber[0].length;
         if(productDetails && relatedProducts){
-            res.render('productDetails.ejs', {product: myProductDetails, relatedProducts: myRelatedProducts})
+            res.render('productDetails.ejs', {product: myProductDetails, relatedProducts: myRelatedProducts, totalCartProducts: totalCartProducts})
         }
         else{
             res.send("There is something error in logic");
@@ -141,6 +154,23 @@ app.post("/seller/add", (req, res)=>{
     }
 })
 
+//Adding product to cart using add to cart button
+app.post('/product/add/:id', (req, res)=>{
+    let productId = req.params.id;
+    let addToCartQuery = 'insert into Cart(productId) values (?)';
+    try{
+        connection.query(addToCartQuery, [productId], (err, result)=>{
+            if(err){
+                res.send("There is something error in adding product to cart using add to cart button");
+            } else{
+                res.redirect(`/product/${productId}`);
+            }
+        })
+    } catch(err){
+        res.send("There is something error in adding product to cart using add to cart button");
+    }
+})
+
 
 //Add to cart
 app.post("/product/:id", (req, res)=>{
@@ -182,6 +212,7 @@ app.delete("/product/:id", (req, res)=>{
 app.patch("/product/:id", (req, res)=>{
     let productId = req.params.id;
     let quantity = req.body.quantity;
+ 
     let updateCartQtyQuery = 'update Cart set productQuantity = ? where productId = ?';
     try{
         connection.query(updateCartQtyQuery, [quantity, productId], (err, result)=>{
