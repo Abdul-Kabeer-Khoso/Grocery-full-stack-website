4ecd2d98209bfb4eee20c41927dd1bdee05cb0dd
Abdul Kabeer <abdulkabeerkhoso082@gmail.com>
2025-07-15 14:22:50 +0500

    Add error handling

--- utils/ExpressError.js
+++ utils/ExpressError.js
@@ -1,7 +1,8 @@
-class ExpressError extends Error {
-    constructor(statusCode, message) {
-        super(message);
+class ExpressError extends Error{
+    constructor(statusCode, message){
+        super();
         this.statusCode = statusCode;
+        this.message = message;
     }
 }
 
